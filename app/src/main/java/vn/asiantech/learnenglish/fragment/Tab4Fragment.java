29fda29c292a3e19ba123f773a91cc115dad55a3
GianhTran <mrgianh92@gmail.com>
2015-09-10 10:27:47 +0700

    New setting tab

--- app/src/main/java/vn/asiantech/learnenglish/fragment/Tab4Fragment.java
+++ app/src/main/java/vn/asiantech/learnenglish/fragment/Tab4Fragment.java
@@ -1,23 +1,48 @@
 package vn.asiantech.learnenglish.fragment;
 
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 import vn.asiantech.learnenglish.R;
+import vn.asiantech.learnenglish.core.BaseFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Tab4Fragment extends Fragment {
 
+
+public class Tab4Fragment extends BaseFragment {
+    private ImageButton imgBtnNotification;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab4, container, false);
+        imgBtnNotification = (ImageButton) view.findViewById(R.id.imgBtnNotificationSettings);
+        imgBtnNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlarmFragment alarmFragment = new AlarmFragment();
+
+                replaceFragment(R.id.frameContain,alarmFragment,"alarmFragment",null);
+
+
+
+            }
+        });
+        return view;
     }
+
 }
